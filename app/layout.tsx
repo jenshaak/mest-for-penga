0924f433c800e44f1beb991d1ea65f43a08083f6
Kevin Stratvert <jens.haakaas@gmail.com>
2024-05-10 18:54:44 +0200

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default async function RootLayout({
       <body className={`${inter.className}`} data-theme="dark">
         <div className="w-full">
           <NavBar />
-          {children}
+          <Suspense fallback={<Loading />}>{children}</Suspense>
         </div>
       </body>
     </html>
