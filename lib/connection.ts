fcc97c0d20d8ee0ca48b2ba664d0779baa896bea
Kevin Stratvert <jens.haakaas@gmail.com>
2024-05-16 11:58:25 +0200

    14

--- lib/connection.ts
+++ lib/connection.ts
@@ -12,11 +12,10 @@ export const connectToDb = async () => {
     console.log("2");
     console.log("MONGODB_URI:", process.env.MONGODB_URI);
     const db = await mongoose.connect(process.env.MONGODB_URI!);
-    console.log("3");
+    console.log("30");
       connection.isConnected = db.connections[0].readyState === 1;
   } catch (error) {
         console.log("Did not connect", error)
       throw new Error(error instanceof Error ? error.message : 'Cannot connect to DataBase');
   }
 };
-    
